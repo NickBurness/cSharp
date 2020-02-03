663a5a3e679350072875b3e8f961a6000f558681
Nicholas Burness <53260549+NickBurness@users.noreply.github.com>
2020-02-03 13:30:24 +0000

    code refactor through encapsulation

--- Pluralsight/gradebook/src/GradeBook/Book.cs
+++ Pluralsight/gradebook/src/GradeBook/Book.cs
@@ -49,16 +49,27 @@ namespace GradeBook
 
         public override void AddGrade(double grade)
         {
-            using(var writer = File.AppendText($"{Name}.txt"))
-            {
-                writer.WriteLine(grade);
-                GradeAdded?.Invoke(this, new EventArgs());
-            }        
+            using var writer = File.AppendText($"{Name}.txt");
+            writer.WriteLine(grade);
+            GradeAdded?.Invoke(this, new EventArgs());
         }
 
         public override Statistics GetStatistics()
         {
-            
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
         }
     }
 
