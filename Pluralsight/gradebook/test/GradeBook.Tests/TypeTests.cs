a928fcb2cbebb734ee96c9d115286fcaa9f9caa0
Nicholas Burness <53260549+NickBurness@users.noreply.github.com>
2020-02-03 11:53:25 +0000

    code refactor

--- Pluralsight/gradebook/test/GradeBook.Tests/TypeTests.cs
+++ Pluralsight/gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -40,7 +40,7 @@ namespace GradeBook.Tests
         [Fact]
         public void DoNotAllowAGradeBelowZeroOrGreaterThanOneHundred()
         {
-            var book = new Book("Book1");
+            var book = new InMemoryBook("Book1");
             Assert.Throws<ArgumentException>(
             () => book.AddGrade(105));
         }
@@ -92,9 +92,9 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void GetBookSetName(ref Book book, string name)
+        private void GetBookSetName(ref InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
         [Fact]
@@ -108,9 +108,9 @@ namespace GradeBook.Tests
             Assert.Equal("Book 1", book1.Name);
         }
 
-        private void GetBookSetName(Book book, string name)
+        private void GetBookSetName(InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
 
@@ -125,7 +125,7 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void SetName(Book book, string name)
+        private void SetName(InMemoryBook book, string name)
         {
             book.Name = name;
         }
@@ -157,9 +157,9 @@ namespace GradeBook.Tests
             Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
-        Book GetBook(string name)
+        InMemoryBook GetBook(string name)
         {
-            return new Book(name);
+            return new InMemoryBook(name);
         }
     }
 }
