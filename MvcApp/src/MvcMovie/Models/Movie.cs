818d0e0da821bfa55bc47c22a099983e34ef4e74
Nicholas Burness <53260549+NickBurness@users.noreply.github.com>
2020-02-04 12:18:25 +0000

    fix syntax error in movie.cs

--- MvcApp/src/MvcMovie/Models/Movie.cs
+++ MvcApp/src/MvcMovie/Models/Movie.cs
@@ -17,7 +17,7 @@ namespace MvcMovie.Models
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), StringLength(30), Required]
         public string Genre { get; set; }
 
-        [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)"]
+        [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
